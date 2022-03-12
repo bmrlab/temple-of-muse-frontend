2ad02d66fed351b06d4160a89d204affb1e600f8
web3nomad.eth <me@web3nomad.eth>
2022-03-12 23:47:23 +0800

    add logo

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -10,11 +10,19 @@ export default function SiteHeader() {
       'justify-between items-center',
       'h-20 px-4'
     )}>
-      <span className='text-grandslang'>Temple Of Muse</span>
-      <button className={clsx(
-        'border border-white hover:border-white/75 hover:text-white/75',
-        'rounded-full px-4 py-1',
-      )}>Connect Wallet</button>
+      <div className='flex flex-row justify-start items-end'>
+        <div className={clsx('w-8 h-8 mr-1', styles.logo)}>{/*logo*/}</div>
+        <span className='text-grandslang text-lg -mb-2'>Temple OF Muse</span>
+      </div>
+      <div className='flex-1'>
+        {/* placeholder */}
+      </div>
+      <div>
+        <button className={clsx(
+          'border border-white hover:border-white/75 hover:text-white/75',
+          'rounded-full px-4 py-1',
+        )}>Connect Wallet</button>
+      </div>
     </header>
   )
 }
