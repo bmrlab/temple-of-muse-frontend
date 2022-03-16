60d7019e5d2563e012cc3cca6a6035b9eebe0785
web3nomad.eth <me@web3nomad.eth>
2022-03-16 17:10:48 +0800

    add space demo

--- components/home/space.tsx
+++ components/home/space.tsx
@@ -4,6 +4,21 @@ import styles from './space.module.css'
 
 export default function Space() {
   return (
-    <div className={clsx(styles.demoCover, 'h-screen', 'relative')}></div>
+    <div className={clsx(
+      // styles.demoCover,
+      'h-screen', 'relative'
+    )}>
+      <iframe
+        src='https://bmrlab.github.io/temple-of-muse-space/'
+        className={clsx(
+          'relative block w-full h-full',
+          'border-none',
+        )}
+      ></iframe>
+      <div className={clsx(
+        'absolute left-0 top-0 w-full h-full',
+        'border-none',
+      )}></div>
+    </div>
   )
 }
