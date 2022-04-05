2cd8c135b155a87aa55a52bd129816c1214ea681
web3nomad.eth <me@web3nomad.eth>
2022-04-06 00:31:06 +0800

    connect nfts drawer to webgl

--- components/temple/nfts-drawer.tsx
+++ components/temple/nfts-drawer.tsx
@@ -1,19 +1,57 @@
 import clsx from 'clsx'
 import { useRecoilValue } from 'recoil'
 import { useEffect, useState, useRef, useCallback } from 'react'
-import NFTs from '@/components/nfts/list'
 import { walletAddressState } from '@/lib/recoil/wallet'
+import { getNFTs, NFTData } from '@/lib/nfts'
 
 
-export default function NFTsDrawer() {
+const NFTList = ({ nfts, onSelectNFT }: { nfts: Array<NFTData>, onSelectNFT: Function }) => {
+  return (
+    <div className='w-full flex items-center justify-center flex-wrap py-12'>
+      {nfts.map((nft) => {
+        return (
+          <div className='block p-2 w-1/2' key={`${nft.contract.address}/${nft.tokenId}`}>
+            <div
+              className='w-full pt-[100%] bg-no-repeat bg-center bg-contain cursor-pointer'
+              style={{'backgroundImage': `url(${nft.mediaUri})`}}
+              onClick={() => onSelectNFT(nft)}
+            ></div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default function NFTsDrawer({ visible, onSelectNFT }: { visible: boolean, onSelectNFT: Function }) {
   const walletAddress = useRecoilValue(walletAddressState)
+
+  let [pending, setPending] = useState<Boolean>(false)
+  let [nfts, setNFTs] = useState<Array<NFTData>>([])
+  let [pageKey, setPageKey] = useState<string|undefined>()
+
+  useEffect(() => {
+    if (!walletAddress) {
+      return
+    }
+    setPending(true)
+    getNFTs(walletAddress).then((data) => {
+      setNFTs([ ...data.results ])
+      setPageKey(data.pageKey)
+      setPending(false)
+    })
+  }, [walletAddress])
+
   return (
     <div className={clsx(
-      'absolute left-0 top-0 w-80 h-full bg-black overflow-auto'
+      'absolute left-0 top-0 w-80 h-full bg-black overflow-auto',
+      'transition-all duration-300 ease-in-out',
+      visible ? 'translate-x-0' : '-translate-x-full'
     )}>
-      {walletAddress ? (
-        <NFTs walletAddress={walletAddress} />
-      ) : (
+      {walletAddress ? <>
+        {pending && <div>loading ...</div>}
+        {!pending && <NFTList nfts={nfts} onSelectNFT={onSelectNFT}/>}
+      </> : (
         <div className='text-center my-12'>Please connect your wallet to view assets.</div>
       )}
     </div>
--- pages/temple/index.tsx
+++ pages/temple/index.tsx
@@ -1,29 +1,47 @@
 import clsx from 'clsx'
 import type { NextLayoutPage } from 'next'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Head from 'next/head'
 import Layout from '@/components/layout'
 import renderWebGL from '@/components/temple/render-webgl'
 import ProgressCover from '@/components/temple/progress-cover'
 import NFTsDrawer from '@/components/temple/nfts-drawer'
 
-
 const Temple: NextLayoutPage = () => {
   let [loadingProgress, setProgress] = useState(0)
+  let [drawerVisible, setDrawerVisible] = useState(false)
+  let [nftSlot, setNFTSlot] = useState('')
 
   useEffect(() => {
     let updateProgress = (progress: number) => setProgress(Math.floor(progress * 100))
     renderWebGL(updateProgress)
+    const listener = (e: any) => {
+      setNFTSlot(e.detail)
+      setDrawerVisible(true)
+    }
+    document.addEventListener('selectSlot', listener)
     return () => {
       updateProgress = () => {}
+      document.removeEventListener('selectSlot', listener)
     }
   }, [])
+
+  const onSelectNFT = useCallback((nft) => {
+    const payload = JSON.stringify({
+      slotkey: nftSlot,
+      imageUrl: nft.mediaUri
+    })
+    const unityInstance = (window as any).unityInstance
+    unityInstance.SendMessage('NFT_Manager', 'SetImage', payload)
+    setDrawerVisible(false)
+  }, [nftSlot])
 
   return (
     <div className='relative'>
       <canvas id='unity-canvas'></canvas>
       <ProgressCover progress={loadingProgress} />
-      <NFTsDrawer />
+      <NFTsDrawer visible={drawerVisible} onSelectNFT={onSelectNFT} />
+      <button className='absolute right-0 top-0' onClick={()=>{setDrawerVisible(true)}}>show</button>
     </div>
   )
 }
