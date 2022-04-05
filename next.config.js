e5625418349c66ee4ae2d2e861afaacb1088e4d0
web3nomad.eth <me@web3nomad.eth>
2022-04-05 14:46:47 +0800

    config customer headers for wasm gzip files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [{
+      source: '/space/Build/(.+\.gz)',
+      headers: [{ key: 'Content-Encoding', value: 'gzip' }]
+    }, {
+      source: '/space/Build/(.+\.wasm\.gz)',
+      headers: [{ key: 'Content-Type', value: 'application/wasm' }]
+    }]
+  }
 }
