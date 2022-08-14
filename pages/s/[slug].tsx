cd6524bcf5509abdfac95521ca1d1a14d3a4d006
web3nomad.eth <me@web3nomad.eth>
2022-08-14 18:51:15 +0800

    add head

--- pages/s/[slug].tsx
+++ pages/s/[slug].tsx
@@ -3,6 +3,7 @@ import { useMemo, useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import Error from 'next/error'
 import Image from 'next/image'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { cdnMediaUri, NFTData } from '@/lib/nfts'
 import { getMobileDetect } from '@/lib/utils'
@@ -11,8 +12,9 @@ import ProgressCover from '@/components/temple/progress-cover'
 import { Space, MediaSlot } from '@prisma/client'
 import prisma from '@/lib/prisma'
 
-type SpaceData = Pick<Space, 'id' | 'ownerAddress' | 'slug' | 'title' | 'description' | 'config' | 'mediaSlots'> & {
-  mediaSlots: Pick<MediaSlot, 'id' | 'slotKey' | 'mediaUri' | 'contractAddress' | 'tokenId' | 'name' | 'description'>[]
+type MediaSlotData = Pick<MediaSlot, 'id' | 'slotKey' | 'mediaUri' | 'contractAddress' | 'tokenId' | 'name' | 'description'>
+type SpaceData = Pick<Space, 'id' | 'ownerAddress' | 'slug' | 'title' | 'description' | 'config'> & {
+  mediaSlots: MediaSlotData[]
 }
 
 async function fillSlots(space: SpaceData) {
@@ -41,7 +43,7 @@ const Page: NextPage<{
   const [loadingProgress, setLoadingProgress] = useState(0)
   const [activeSlotKey, setActiveSlotKey] = useState('')
 
-  const activeMediaSlot = useMemo<MediaSlot|null>(() => {
+  const activeMediaSlot = useMemo<MediaSlotData|null>(() => {
     if (!activeSlotKey) {
       return null
     }
@@ -84,7 +86,7 @@ const Page: NextPage<{
     }
   }, [space, loadingProgress])
 
-  const MediaDetailDrawer = ({ activeMediaSlot }: { activeMediaSlot: MediaSlot }) => {
+  const MediaDetailDrawer = ({ activeMediaSlot }: { activeMediaSlot: MediaSlotData }) => {
     return <div className='fixed left-0 top-0 w-full h-full flex items-center justify-center'>
       <div className='w-3/4 lg:w-2/3 h-3/4 text-white relative'>
         {/* black bg with rounded corner */}
@@ -125,33 +127,43 @@ const Page: NextPage<{
   if (mobileDetect.isSSR()) {
     return <div></div>
   } else if (mobileDetect.isMobile() && !ignoreMobile) {
-    return <div className='min-h-screen flex flex-col items-center justify-center bg-black text-white'>
-      <div
-        className='w-12 h-12 my-4'
-        style={{
-          'background': 'url("/static/logo-white.png") no-repeat center',
-          'backgroundSize': 'contain',
-        }}
-      ></div>
-      <div className='text-grandslang text-3xl my-2'>Temple OF Muse</div>
-      <div className='px-4 my-2 text-center'>Not available in mobile yet, please open in desktop.</div>
-      <a
-        className='py-3 px-16 my-6 bg-white text-black rounded-full flex flex-row items-center'
-        href='https://bmr.art'
-      >
-        <span className='mr-2'>Back to BMR</span>
-        <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10.4812 2.35418L3.27839 2.35418V0L14.5 0V11.2216H12.1458V4.01884L2.16466 14L0.5 12.3353L10.4812 2.35418Z" fill="black"/>
-        </svg>
-      </a>
-    </div>
+    return (
+      <div className='min-h-screen flex flex-col items-center justify-center bg-black text-white'>
+        <div
+          className='w-12 h-12 my-4'
+          style={{
+            'background': 'url("/static/logo-white.png") no-repeat center',
+            'backgroundSize': 'contain',
+          }}
+        ></div>
+        <div className='text-grandslang text-3xl my-2'>Temple OF Muse</div>
+        <div className='px-4 my-2 text-center'>Not available in mobile yet, please open in desktop.</div>
+        <a
+          className='py-3 px-16 my-6 bg-white text-black rounded-full flex flex-row items-center'
+          href='https://bmr.art'
+        >
+          <span className='mr-2'>Back to BMR</span>
+          <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.4812 2.35418L3.27839 2.35418V0L14.5 0V11.2216H12.1458V4.01884L2.16466 14L0.5 12.3353L10.4812 2.35418Z" fill="black"/>
+          </svg>
+        </a>
+      </div>
+    )
   } else {
-    return <div className='relative min-h-screen'>
-      <canvas id='unity-canvas'></canvas>
-      <ProgressCover loadingProgress={loadingProgress} />
-      {activeMediaSlot && <MediaDetailDrawer activeMediaSlot={activeMediaSlot} />}
-    </div>
+    return (
+      <div className='relative min-h-screen'>
+        <Head>
+          <title>{space.title}</title>
+          <meta name='description' content={space.description ?? ''} />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <canvas id='unity-canvas'></canvas>
+        <ProgressCover loadingProgress={loadingProgress} />
+        {activeMediaSlot && <MediaDetailDrawer activeMediaSlot={activeMediaSlot} />}
+      </div>
+    )
   }
+
 }
 
 export async function getServerSideProps({ query }: {
@@ -160,7 +172,7 @@ export async function getServerSideProps({ query }: {
   const slug = (query.slug ?? '').toString()
   const m = (query.m ?? '').toString()
   // axios.get(`/api/spaces/${slug}`)
-  const space: SpaceData = await prisma.space.findUnique({
+  const space: SpaceData|null = await prisma.space.findUnique({
     where: { slug },
     select: {
       id: true,
