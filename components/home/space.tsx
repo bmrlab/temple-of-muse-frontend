b1b2a7d5f3ef5b21abf6cb32ac9eb80b94d0fa86
web3nomad.eth <me@web3nomad.eth>
2022-08-21 18:40:35 +0800

    improve homepage space on mobile

--- components/home/space.tsx
+++ components/home/space.tsx
@@ -6,13 +6,16 @@ import imageArch from '../../assets/images/space-demo-cover.jpg'
 export default function Space() {
   return (
     <div
-      className='bg-no-repeat bg-center bg-cover relative'
-      style={{'backgroundImage': `url(${imageArch.src})`, 'paddingTop': '48%'}}
+      className={clsx(
+        'bg-no-repeat bg-center bg-cover relative',
+        'h-screen lg:h-auto lg:pt-[48%]'
+      )}
+      style={{'backgroundImage': `url(${imageArch.src})`}}
     >
       <div
         className={clsx(
           'absolute opacity-50 font-["helvetica"] text-xs',
-          'left-0 right-0 bottom-2 text-center',
+          'left-0 right-0 bottom-4 text-center',
           'lg:left-3 lg:bottom-2 lg:right-auto lg:text-left',
           'xl:left-6 xl:bottom-4',
         )}
